162e470ec9dc7d937d0e00ee1d052de6e96f0502
44797 <44797@192.168.0.100>
2026-07-23 07:39:22 +0100

    dddd

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -1,12 +1,20 @@
 
 
+
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
-//////////////////////////////////////////////////////////////////
-//
+import MOApplication.Surface;
 
+
+/**
+ * Main Application JFrame class. Contains the "main()" method. Instantiates the UserSession
+ * which is itself derived from Surface, which runs the updating and control flow of the session.
+ * 
+ */
+@SuppressWarnings("serial")
 public class ApplicationMain extends JFrame {
 
     public ApplicationMain() {
